c75ec4cf1d2e054f60d9bca2ee285a9da10160da
BAKEZQ <zhongquan789@gmail.com>
2021-05-19 11:54:45 +0800

    Update viwer.tsx

--- src/components/method/grid/viwer.tsx
+++ src/components/method/grid/viwer.tsx
@@ -52,6 +52,10 @@ export default function Viewer(props) {
     }
   }, [data])
   
+  useEffect(() => {
+    onView()
+  }, [model, gr1, gr2])
+  
   const onView = () => {
     if (!model || !gr1 || !gr2) return
     setLoading(true)
